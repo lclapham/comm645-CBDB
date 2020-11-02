c7128c4e849329d1aa018a656694669453175169
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-11-02 13:52:38 -0800

    Initial Project Launch

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Simple return to last page -->
+function goBack() {
+    window.history.back();
+}
+
+
+// Git rid of this later. Simple redirect for login page testing
+function tempLogin() {
+    document.location.href="#pgHome"; 
+}
